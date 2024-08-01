deef48f3dd85595a735cd703b779a66b49aa906c
Dogbonee <hundeknochen57@gmail.com>
2024-08-01 14:10:02 -0600

    Add includes

--- include/Dualie/Dualie.hpp
+++ include/Dualie/Dualie.hpp
@@ -1,7 +1,7 @@
 #ifndef DUALIE_DUALIE_HPP
 #define DUALIE_DUALIE_HPP
 #include <Dualie/Graphics.hpp>
-#include <Dualie/System/Input.hpp>
+#include <Dualie/System.hpp>
 #include <Dualie/Audio.hpp>
 
 #endif //DUALIE_DUALIE_HPP
---
+++ include/Dualie/System.hpp
@@ -0,0 +1,12 @@
+//
+// Created by caleb on 8/1/24.
+//
+
+#ifndef DUALIE_SYSTEM_HPP
+#define DUALIE_SYSTEM_HPP
+#include <Dualie/System/Input.hpp>
+#include <Dualie/System/Clock.hpp>
+#include <Dualie/System/Rect.hpp>
+#include <Dualie/System/Time.hpp>
+#include <Dualie/System/Vector2.hpp>
+#endif //DUALIE_SYSTEM_HPP
