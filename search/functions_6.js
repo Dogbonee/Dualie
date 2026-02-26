ccb8d20cedc71fb189cffbac31ab71f9adc4280e
Dogbonee <76922975+Dogbonee@users.noreply.github.com>
2026-02-26 00:19:44 +0000

    Deploying to gh-pages from @ Dogbonee/Dualie@2488da838df2be36bc474b2c9258cad2387a3e01 🚀

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['loadfromfile_0',['loadfromfile',['../classdl_1_1Music.html#afa06f967812496c67c4b6a4c13d6ce8c',1,'dl::Music::loadFromFile()'],['../classdl_1_1SpriteSheet.html#a4acefffe3a0ffff8f393dd90940271de',1,'dl::SpriteSheet::loadFromFile()']]],
+  ['loadfromfile_0',['loadfromfile',['../classdl_1_1Music.html#a60d6ab42aac088172db73a3be708cb81',1,'dl::Music::loadFromFile()'],['../classdl_1_1SpriteSheet.html#a4acefffe3a0ffff8f393dd90940271de',1,'dl::SpriteSheet::loadFromFile()']]],
   ['loadfromspritesheet_1',['loadFromSpriteSheet',['../classdl_1_1Sprite.html#ae80e1c70e17a41738637b0fc2b4c36db',1,'dl::Sprite']]]
 ];
